7f8c8b7b90af330380d00c66b3ef816625072216
chau <chau.nm194420@sis.hust.edu.vn>
2021-02-27 21:22:35 +0700

    update

---
+++ Excercises/src/chau/ex1.java
@@ -0,0 +1,15 @@
+package chau;
+import java.util.Scanner;
+public class ex1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("first number: ");
+		double num1 = scanner.nextDouble();
+		System.out.print("second number: ");
+		double num2 = scanner.nextDouble();
+		System.out.println("the sum is: "+ (num1+num2));
+		System.out.println("the difference is: "+ (num1 -num2));
+		
+	}
+
+}
