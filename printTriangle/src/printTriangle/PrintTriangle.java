c1ff49d0c4e994bad298a1346b52508a23c67943
chau25102001 <chau.nm194420@sis.hust.edu.vn>
2021-03-01 09:18:13 +0700

    new update

--- printTriangle/src/printTriangle/PrintTriangle.java
+++ printTriangle/src/printTriangle/PrintTriangle.java
@@ -1,16 +1,16 @@
-package chau;
+package printTriangle;
 import java.util.Scanner;
-public class ex3 {
+public class PrintTriangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("enter n: ");
 		int n = scanner.nextInt();
 		for (int i =0; i<n; i++) {
-			System.out.println(" ".repeat(n-i)+"*".repeat(2*i+1)+" ".repeat(n-i));
-			
+		System.out.println(" ".repeat(n-i)+"*".repeat(2*i+1)+" ".repeat(n-i));
+					
 		}
+
 	}
 
-}//a
+}
